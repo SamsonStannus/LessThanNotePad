382e9bdfff8bb619d02583d851ec0bfe2505ac3e
SamsonStannus <samsonstannus@gmail.com>
2015-01-22 15:42:47 -0500

    adding java files

---
+++ src/text_editor/TextEditor.java
@@ -0,0 +1,16 @@
+package text_editor;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+import javax.swing.text.*;
+
+
+
+class TextEditor extends JFrame {
+	
+
+
+private JTextArea area = new JTextArea(20,120);
+private JFileChooser dialog = new JFileChooser(System.getProperty("user.dir"));
+}
