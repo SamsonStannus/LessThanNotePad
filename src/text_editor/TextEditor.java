74f2156facf4381d99d033110534c354a7187ca2
SamsonStannus <samsonstannus@gmail.com>
2015-01-23 14:04:55 -0500

    Everything is working, except new

--- src/text_editor/TextEditor.java
+++ src/text_editor/TextEditor.java
@@ -31,8 +31,8 @@ class TextEditor extends JFrame {
 		file.add(New);
 		file.add(Open);
 		file.add(Save);
-		file.add(Quit);
 		file.add(SaveAs);
+		file.add(Quit);
 		file.addSeparator();
 		
 		for(int i=0; i<4; i++){
@@ -56,9 +56,9 @@ class TextEditor extends JFrame {
 			
 			JButton cut = tool.add(Cut), cop = tool.add(Copy), pas = tool.add(Paste);
 			
-			cut.setText(null); cut.setIcon(new ImageIcon("cut.png"));
-			cop.setText(null); cop.setIcon(new ImageIcon("copy.png"));
-			pas.setText(null); pas.setIcon(new ImageIcon("paste.png"));
+			cut.setText(null); cut.setIcon(new ImageIcon("src/resources/cut.png"));
+			cop.setText(null); cop.setIcon(new ImageIcon("src/resources/copy.png"));
+			pas.setText(null); pas.setIcon(new ImageIcon("src/resources/paste.png"));
 			
 			Save.setEnabled(false);
 			SaveAs.setEnabled(false);
@@ -69,10 +69,109 @@ class TextEditor extends JFrame {
 			setTitle(currentFile);
 			setVisible(true);
 			
-		
-		
-		
-		
+		}
 	
+	private KeyListener k1 = new KeyAdapter() {
+		public void keyPressed(KeyEvent e){
+			changed = true;
+			Save.setEnabled(true);
+			SaveAs.setEnabled(true);
+		}
+	};
+	
+	Action Open = new AbstractAction("Open", new ImageIcon("src/resources/open.png")) {
+		public void actionPerformed(ActionEvent e) {
+			saveOld();
+			if(dialog.showOpenDialog(null)==JFileChooser.APPROVE_OPTION) {
+				readInFile(dialog.getSelectedFile().getAbsolutePath());
+			}
+			SaveAs.setEnabled(true);
+		}
+	};
+	
+	Action New = new AbstractAction("New", new ImageIcon("src/resources/new.png")) {
+		public void actionPerformed(ActionEvent e) {
+//			saveOld();
+//			if(dialog.showOpenDialog(null)==JFileChooser.APPROVE_OPTION) {
+//				readInFile(dialog.getSelectedFile().getAbsolutePath());
+//			}
+//			SaveAs.setEnabled(true);
+			
+			new TextEditor();
+		}
+	};
+	
+	Action Save = new AbstractAction("Save", new ImageIcon("src/resources/save.png")) {
+		public void actionPerformed(ActionEvent e) {
+			if(!currentFile.equals("Untitled"))
+				saveFile(currentFile);
+			else
+				saveFileAs();
+		}
+	};
+	
+	Action SaveAs = new AbstractAction("Save as...") {
+		public void actionPerformed(ActionEvent e) {
+			saveFileAs();
+		}
+	};
+	
+	Action Quit = new AbstractAction("Quit") {
+		public void actionPerformed(ActionEvent e) {
+			saveOld();
+			System.exit(0);
+		}
+	};
+	
+	ActionMap m = area.getActionMap();
+		Action Cut = m.get(DefaultEditorKit.cutAction);
+		Action Copy = m.get(DefaultEditorKit.copyAction);
+		Action Paste = m.get(DefaultEditorKit.pasteAction);
+		
+	private void saveFileAs(){
+		if(dialog.showSaveDialog(null) == JFileChooser.APPROVE_OPTION)
+			saveFile(dialog.getSelectedFile().getAbsolutePath());
 	}
+		
+	private void saveOld(){
+		if(changed) {
+			if(JOptionPane.showConfirmDialog(this, "Would you like to save "+ currentFile+ " ?", "Save",JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
+				saveFile(currentFile);
+		}
+	}
+	
+	private void readInFile(String fileName){
+		try {
+			FileReader r = new FileReader(fileName);
+			area.read(r,null);
+			r.close();
+			currentFile = fileName;
+			setTitle(currentFile);
+			changed = false;
+		}
+		catch(IOException e) {
+			Toolkit.getDefaultToolkit().beep();
+			JOptionPane.showMessageDialog(this, "Editor can't find the file called "+ fileName);
+		}
+	}
+	
+	private void saveFile(String fileName){
+		try {
+			FileWriter w = new FileWriter(fileName);
+			area.write(w);
+			w.close();
+			currentFile = fileName;
+			setTitle(currentFile);
+			changed = false;
+			Save.setEnabled(false);
+		}
+		catch(IOException e) {
+			
+		}
+	}
+	
+	public static void main(String[] arg){
+		new TextEditor();
+	}
+	
 }
